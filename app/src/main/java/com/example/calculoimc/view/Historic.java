1d1016269272a05b52f0c1e074d0b80c0289014b
Lucas <mendes.lucasmendes@gmail.com>
2026-04-08 12:18:36 -0300

    Criando exibição inicial dos graficos

--- app/src/main/java/com/example/calculoimc/view/Historic.java
+++ app/src/main/java/com/example/calculoimc/view/Historic.java
@@ -2,15 +2,18 @@ package com.example.calculoimc.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import com.example.calculoimc.R;
 import com.example.calculoimc.database.DataBase;
-import com.example.calculoimc.model.IMC;
 import com.example.calculoimc.model.UserAtributos;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import java.util.ArrayList;
 
 import java.util.List;
 
@@ -26,6 +29,9 @@ public class Historic extends AppCompatActivity {
 
         // Busca a lista de IMCs do banco
         List<UserAtributos> listaImc = db.getAllUsersWithIMC();
+
+
+        gerarGrafico(listaImc);
 
         // Cria um adaptador simples (usa o toString() da sua classe IMC)
         ArrayAdapter<UserAtributos> adapter = new ArrayAdapter<>(
@@ -49,6 +55,30 @@ public class Historic extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void gerarGrafico(List<UserAtributos> listaImc) {
+        LineChart chart = findViewById(R.id.chartHistorico);
+        List<Entry> entradas = new ArrayList<>();
+
+// Preenchemos os dados do gráfico
+        for (int i = 0; i < listaImc.size(); i++) {
+            float imcValor = (float) listaImc.get(i).getImc().getIndice();
+            entradas.add(new Entry(i, imcValor));
+        }
+
+// Configuração visual do gráfico
+        LineDataSet dataSet = new LineDataSet(entradas, "Evolução do IMC");
+        dataSet.setColor(Color.BLUE);
+        dataSet.setCircleColor(Color.DKGRAY);
+        dataSet.setLineWidth(2f);
+        dataSet.setValueTextSize(10f);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.getDescription().setText("Seu progresso");
+        chart.animateX(1000); // Animação de entrada
+        chart.invalidate(); // Refresh
     }
 
 }
